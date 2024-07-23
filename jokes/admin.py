823d40b9549368e6bcba2d4349547b3d763811a1
kjdwebdev <kdicampo.webdev@gmail.com>
2024-07-23 15:41:10 -0400

    added joke admin

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,3 +1,15 @@
+from typing import Any
 from django.contrib import admin
+from django.http import HttpRequest
+from .models import Joke
 
 # Register your models here.
+@admin.register(Joke)
+class JokeAdmin(admin.ModelAdmin):
+    model = Jokelist_display = ['question', 'created', 'updated']
+
+    def get_readonly_fields(self, request, obj: None):
+        if obj: #editing an existing object
+            return ('created', 'updated')
+    
+        return()
