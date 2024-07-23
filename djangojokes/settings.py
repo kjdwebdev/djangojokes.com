76bc7cc99bfda5f7eb5a59d0290cc77f60ec2abe
kjdwebdev <kdicampo.webdev@gmail.com>
2024-07-23 15:54:07 -0400

    added admin doc generator

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #Built-in django apps
     'django.contrib.admin',
+    'django.contrib.admindocs',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
