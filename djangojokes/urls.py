9395f172147088c7914f7dd9bd6faa7ed1cc8b46
kjdwebdev <kdicampo.webdev@gmail.com>
2024-08-05 15:15:20 -0400

    Getting Started with django-allauth

--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -18,8 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    #Admin
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
+
+    #User management
+    path('account/', include('allauth.urls')),
+
+    #Local apps
     path('jobs/', include('jobs.urls')),
     path('jokes/', include('jokes.urls')),
     path('', include('pages.urls')),
