df654641dfe1b7d63e2c6bcc49dd47ba5b5a29e8
kjdwebdev <kdicampo.webdev@gmail.com>
2024-08-14 10:39:39 -0400

    register jokevotes model in admin

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,9 +1,19 @@
 from typing import Any
 from django.contrib import admin
 from django.http import HttpRequest
-from .models import Category, Joke, Tag
+from .models import Category, Joke, JokeVote, Tag
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ['category', 'created', 'updated']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return ('slug', 'created', 'updated')
+        return ()
+
 @admin.register(Joke)
 class JokeAdmin(admin.ModelAdmin):
     model = Jokelist_display = ['question', 'created', 'updated']
@@ -14,14 +24,14 @@ class JokeAdmin(admin.ModelAdmin):
     
         return()
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    model = Category
-    list_display = ['category', 'created', 'updated']
+@admin.register(JokeVote)
+class JokeVoteAdmin(admin.ModelAdmin):
+    model = JokeVote
+    list_display = ['joke', 'user', 'vote']
 
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
-            return ('slug', 'created', 'updated')
+            return ('created', 'updated')
         return ()
 
 @admin.register(Tag)
